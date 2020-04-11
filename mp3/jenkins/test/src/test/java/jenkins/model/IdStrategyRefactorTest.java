af1cd39884bd87b59ed6961c08376f09e5107381
Sam Wise <57547641+wisey1@users.noreply.github.com>
2020-04-10 22:42:33 -0400

    Add files via upload

---
+++ mp3/jenkins/test/src/test/java/jenkins/model/IdStrategyRefactorTest.java
@@ -0,0 +1,12 @@
+package jenkins.model;
+
+public class IdStrategyRefactorTest
+{
+		@Test
+		public void idFromFileName(String id) throws Exception
+		{
+			//it looks like in order to reach
+			//the section of code that we had to work with/refactor,
+			//we need to test 2 string values that each contain a '$'
+		}		
+}
